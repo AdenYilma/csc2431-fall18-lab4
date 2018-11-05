27bafbde57caff44f4e3f4cc884671d2db1b4e79
Aden Yilma <aden@Adens-MacBook-Air.local>
2018-11-05 15:12:31 -0800

    1st commit

--- recur.cc
+++ recur.cc
@@ -3,17 +3,23 @@
 //TODO #1.1: add a function prototype for fib
 
 //TODO #2.1: add a function prototype for fact
+int fib(int);
 
 int main()
 {
 	//TODO: #1.2 call your fib function, print the result
-	
+    fib(5);
 	//TODO: #2.2 call your fact function, print the result
 
 	return 0;
 }
 
 //TODO #2.3: add your recursive fib function from class
-
+int fib(int n){
+    return n;
+}
 
 //TODO #2.4: add your recursive fact function from class
+int fib(int n){
+    return n;
+}
